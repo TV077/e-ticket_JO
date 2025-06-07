d9a320f78631b0c87f68a490e974d440792ee62e
Thibaud Vadi <thibaudvadi@iMac.lan>
2025-06-07 20:24:20 +0200

    BDD

--- offers/models.py
+++ offers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Offer(models.Model):
+    OFFER_TYPE = [
+        ('solo', 'Solo'),
+        ('duo','Duo'),
+        ('famille', 'Famille'),
+    ]
+
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(max_length=10, choices=OFFER_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
+from symtable import Class
+
 from django.db import models
+from django.conf import settings
+from offers.models import Offer
+import uuid
 
 # Create your models here.
+
+#Commande d'un utilisateur
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    ticket_key = models.UUIDField(default=uuid.uuid4, editable=False)
+
+#Ticket généré à la suite d'une commande
+    def __str__(self):
+        return f"Commande de {self.user} - {self.offer.name}"
+
+class Ticket(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    final_key = models.CharField(max_length=255) #clé concaténée  = uuid_user + ticket_key
+    is_valid = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Ticket - {self.order.user.username} - ({'valide' if self.is_valid else 'utilisé'})"
