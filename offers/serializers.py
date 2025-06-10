dd4dd7c6b1e70f01be44ad6241148629b9edad5e
Thibaud Vadi <thibaudvadi@iMac.lan>
2025-06-10 19:47:19 +0200

    View Offer

--- offers/serializers.py
+++ offers/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Offer
 
-class offerSerializer(serializers.ModelSerializer):
+class OfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Offer
         fields = '__all__'
--- offers/views.py
+++ offers/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .models import Offer
+from .serializers import OfferSerializer
+from rest_framework.permissions import AllowAny
+
 # Create your views here.
+#ModelViewSet fournit automatiquement GET,POST,PUT,DELETE
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = [AllowAny]  #Autorisation
+
